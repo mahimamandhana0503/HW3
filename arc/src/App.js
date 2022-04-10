ed2a100c15140bd3e10c45a481e0db7039eb768d
Mahima Mandhana <mandhana.mahima@gmail.com>
2022-04-09 19:29:34 -0700

    Initial Commit for HW3

---
+++ arc/src/App.js
@@ -0,0 +1,22 @@
+import Campus from './Campus'; 
+import React from 'react';
+import * as ReactDOM from 'react-dom';
+import {Map} from '@esri/react-arcgis';
+
+function App() {
+ ReactDOM.render(
+  <Map /> ,
+  document.getElementById('container')
+ );// ReactDOM.render()
+}// App()
+
+export default (props) => (
+    <Map style={{ width: '70vw', height: '90vh' }}
+        //mapProperties={{ basemap: 'satellite' }}
+        viewProperties={{
+            center: [-118.28538,34.025],
+            zoom: 14
+        }}>
+        <Campus />
+    </Map>
+)
